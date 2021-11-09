6c9a1a749f360fe437f6f25706dc2e876302bd1b
Edgar Manuel Peralta Sánchez <edperalta.c02@ucamp.utel.edu.mx>
2021-11-09 09:03:48 -0700

    Inicio de proyecto 4 restaurant app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu';
+import List from './List';
+
+function App() {
+  return (
+  <div className="app">
+    <Menu title="Amozon"/>
+    <List />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Menu(props) {
+  return (
+    <div className="container">
+      <div className="subcontainer">
+        <div className="logo">
+          {props.title}
+        </div>
+
+        <div className="search">
+
+        </div>
+
+        <div className="actions">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
