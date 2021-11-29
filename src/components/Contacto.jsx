2f27f1fc0b617b7bf003ca72939208337dd9fbab
Edgar Manuel Peralta Sánchez <edperalta.c02@ucamp.utel.edu.mx>
2021-11-28 20:43:04 -0700

    Modificando Contacto y Nosotros jsx

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -4,6 +4,8 @@ const Contacto = () => {
   return(
     <div>
         <h1>Contacto</h1>
+        <p>Para contactarnos visitanos en www.fleckfries.com.mx o bien
+          mandanos un correo a fleckfries@gmail.com</p>
     </div>
   )
 }
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nosotros = () => {
-
-  const [equipo, setEquipo] = React.useState([])
+  const [equipo, setEquipo] = React.useState([]);
 
   React.useEffect(() => {
-    obtenerDatos()
-  }, [])
+    obtenerDatos();
+  }, []);
 
   const obtenerDatos = async () => {
-    const data = await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
-    const users = await data.json()
-    setEquipo(users.civilizations)
-  }
+    const data = await fetch(
+      "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
+    );
+    const users = await data.json();
+    setEquipo(users.civilizations);
+  };
   return (
     <div>
       <h1>Nosotros</h1>
+      <p>
+        Somos una compañía mexicana-holandesa con 6 años de trayectoria, comprometida a ofrecerte un snack
+        para tí y tu familia, con un toque mexicano-europeo de papas fritas.
+      </p>
       <ul>
-        {
-          equipo.map(item => (
-            <li key={item.id}>
-              <Link to ={` /nosotros/${item.id}` }>
-                  {item.name} - {item.expansion}
-              </Link>
-              
-              </li>
-          ))
-        }
+        {equipo.map((item) => (
+          <li key={item.id}>
+            <Link to={` /nosotros/${item.id}`}>
+              {item.name} - {item.expansion}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Nosotros
+export default Nosotros;
